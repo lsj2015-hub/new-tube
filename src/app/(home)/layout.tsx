c2fa60d091d02b8a74f5fc2d9babc50e680b654d
lsj2015-hub <lsj2015@hotmail.com>
2025-02-13 18:30:50 +0800

    tRPC setup

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,16 @@
 import { HomeLayout } from '@/modules/home/ui/layout/home-layout';
+import { TRPCProvider } from '@/trpc/client';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <HomeLayout>{children}</HomeLayout>;
+  return (
+    <HomeLayout>
+      <TRPCProvider>{children}</TRPCProvider>
+    </HomeLayout>
+  );
 };
 
 export default Layout;
